881070a1480be8edfefe122b74704d7ea408b8f6
Sam Davies <sam@razeware.com>
2020-05-13 11:39:43 +0100

    WEB-3255: Updating the CLI to support the new metadata file

--- app/commands/robles_cli.rb
+++ app/commands/robles_cli.rb
@@ -2,27 +2,17 @@
 
 # Overall CLI app for robles
 class RoblesCli < Thor
-  desc 'hello NAME', 'say hello to NAME'
-  options from: :required, yell: :boolean
-  def hello(name)
-    output = []
-    output << "from: #{options[:from]}" if options[:from]
-    output << "Hello #{name}"
-    output = output.join("\n")
-    puts options[:yell] ? output.upcase : output
-  end
-
-  desc 'render CODEX_FILE', 'renders books from CODEX_FILE'
+  desc 'render [PUBLISH_FILE]', 'renders book from PUBLISH_FILE. [Default: /data/src/publish.yaml]'
   options output: :string
-  def render(codex_file)
-    output = options[:output] || '/data/output'
-    book = Renderer::Book.render(codex_file)
+  def render(publish_file = '/data/src/publish.yaml')
+    # output = options[:output] || '/data/output'
+    book = Renderer::Book.render(publish_file)
     p book
   end
 
-  desc 'publish CODEX_FILE', 'renders and published a book from CODEX_FILE'
-  def publish(codex_file)
-    book = Renderer::Book.render(codex_file)
-    p Api::Alexandria::BookUploader.upload(book)
+  desc 'publish [PUBLISH_FILE]', 'renders and publishes a book from PUBLISH_FILE. [Default: /data/src/publish.yaml]'
+  def publish(publish_file = '/data/src/publish.yaml')
+    book = Renderer::Book.render(publish_file)
+    Api::Alexandria::BookUploader.upload(book)
   end
 end
