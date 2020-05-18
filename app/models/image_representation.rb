3e2aac675608d5d14f465987cda5701c24dec129
Sam Davies <sam@razeware.com>
2020-05-18 23:17:31 +0100

    WEB-3253: Creating a simple image uploader

--- app/models/image_representation.rb
+++ app/models/image_representation.rb
@@ -11,16 +11,29 @@ class ImageRepresentation
     original: nil
   }.freeze
 
-  attr_accessor :width, :remote_url, :local_url, :image
+  attr_accessor :width, :local_url, :root_path, :uploaded, :image
 
   validates :width, inclusion: { in: WIDTHS.keys }, presence: true
   validates :image, presence: true
+  validates :root_path, presence: true
 
   def filename
     "#{image.key}/#{width_px}w#{image.extension}"
   end
 
+  def key
+    raise 'Invalid representation' unless valid?
+
+    "#{root_path}/#{filename}"
+  end
+
   def width_px
     WIDTHS[width]
+  end
+
+  def remote_url
+    return unless uploaded && IMAGES_CDN_HOST.present?
+
+    "https://#{IMAGES_CDN_HOST}/#{key}"
   end
 end
