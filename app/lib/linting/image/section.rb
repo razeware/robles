99e805dc90bdef4e5f09911d09d4c1b9f3af06ea
Sam Davies <sam@razeware.com>
2020-06-12 12:07:46 +0100

    WEB-3417: Adding linting for attachable images

--- app/lib/linting/image/section.rb
+++ app/lib/linting/image/section.rb
@@ -4,23 +4,20 @@ module Linting
   module Image
     # Lint the image associated with a section
     class Section
+      include Linting::Image::Attachable
       include Linting::Image::Markdown
 
-      attr_reader :section
-      delegate :markdown_file, to: :section
+      delegate :markdown_file, to: :object
 
       def self.lint(section)
-        new(section: section).lint
-      end
-
-      def initialize(section:)
-        @section = section
+        new(section).lint
       end
 
       def lint
         [].tap do |annotations|
+          annotations.concat(lint_attachments)
           annotations.concat(lint_markdown_images)
-          section.chapters.each do |chapter|
+          object.chapters.each do |chapter|
             annotations.concat(Linting::Image::Chapter.lint(chapter))
           end
         end
