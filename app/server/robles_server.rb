a73803278b8a551e97c482a2c909cd8dd6237796
Sam Davies <sam@razeware.com>
2020-08-07 11:46:14 +0100

    WEB-3560: Got templated page to render for book

--- app/server/robles_server.rb
+++ app/server/robles_server.rb
@@ -2,7 +2,20 @@
 
 # A local preview server for robles
 class RoblesServer < Sinatra::Application
+  set :bind, '0.0.0.0'
+  set :views, __dir__ + '/views'
+
   get '/' do
-    'Hello world!'
+    erb :'index.html', locals: { book: book, title: "robles Preview: #{book.title}" }, layout: :'layout.html'
+  end
+
+  def book
+    parser = Parser::Publish.new(file: publish_file)
+    book = parser.parse
+    book
+  end
+
+  def publish_file
+    '/data/src/publish.yaml'
   end
 end
