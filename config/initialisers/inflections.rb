df49b5f359c662e294346ca18a76071244efdff4
Sam Davies <sam@razeware.com>
2021-09-06 17:15:45 +0100

    WEB-4315: Improving some inflections

---
+++ config/initialisers/inflections.rb
@@ -0,0 +1,6 @@
+ActiveSupport::Inflector.inflections do |inflect|
+  %w[
+      RW iPhone iPad RxSwift macOS UI tvOS TV MacBook LinkedIn GCD 
+      raywenderlich OSX SceneKit SpriteKit CV
+    ].each { |acronym| inflect.acronym(acronym) }
+end
