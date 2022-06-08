322ae7f3b47ee9e18855956c7088d5a1cf2b6fcf
Joan Nyambura <njugunajoan35@gmail.com>
2022-06-08 14:14:38 +0300

    WEB-5072: Create methods to open and extract vtt file

--- app/lib/renderer/episode.rb
+++ app/lib/renderer/episode.rb
@@ -7,10 +7,31 @@ module Renderer
     include MarkdownRenderable
     include Util::Logging
 
+    attr_reader :object, :vtt_path
+
+    def initialize(object, vtt_path)
+      @object = object
+      @vtt_path = vtt_path
+    end
+
     def render
       logger.info "Beginning episode render: #{object.ordinal}: #{object.title}"
       attach_images
       render_markdown
+    end
+
+    # Extract vtt file
+    def generate_vtt_text
+      vtt_data = vtt.read
+      vtt_data.cues.each do |cue|
+        logger.info "Text: #{cue.text}"
+        logger.info "--"
+      end
+    end
+
+    # Open vtt file
+    def vtt
+      @vtt = WebVTT::File.open(vtt_path) if vtt_path.present?
     end
   end
 end
