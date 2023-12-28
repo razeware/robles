0097f7a21f48f1424be6b4c3ec9043c0225c9f5a
Kapil Sachdev <kapilsachdev03@gmail.com>
2023-12-28 14:12:13 +0530

    WEB-6625: Add free attribute to Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,7 +7,7 @@ class Lesson
   include Concerns::ImageAttachable
   include Concerns::MarkdownRenderable
 
-  attr_accessor :title, :description_md, :ordinal, :ref, :segments, :learning_objectives_md
+  attr_accessor :title, :description_md, :ordinal, :ref, :segments, :learning_objectives_md, :free
 
   attr_markdown :description, source: :description_md, file: false
   attr_markdown :learning_objectives, source: :learning_objectives_md, file: false
@@ -15,6 +15,7 @@ class Lesson
 
   def initialize(attributes = {})
     super
+    @free ||= false
     @segments ||= []
   end
 
@@ -24,7 +25,7 @@ class Lesson
 
   # Used for serialisation
   def attributes
-    { title: nil, description: nil, learning_objectives: nil, ordinal: nil, segments: [], ref: nil }.stringify_keys
+    { title: nil, description: nil, learning_objectives: nil, ordinal: nil, segments: [], ref: nil, free: false }.stringify_keys
   end
 
   # Used for linting
