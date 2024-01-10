a0d7ed2078518cf03d56a4cd2c66b2bcdb9d653f
Sam Davies <sam@kodeco.com>
2024-01-10 15:41:30 +0700

    WEB-6757: Improving lesson and segment validation

--- app/models/segments_validator.rb
+++ app/models/segments_validator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # A validator that will check an array of choices for uniqueness of ref
-class LessonsValidator < ActiveModel::EachValidator
+class SegmentsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value.is_a?(Array)
 
@@ -11,8 +11,8 @@ class LessonsValidator < ActiveModel::EachValidator
   end
 
   def check_correct_class(record, attribute, value)
-    value.each do |choice|
-      record.errors.add(attribute, "lesson #{choice} is not a Lesson") unless choice.is_a?(Lesson)
+    value.each do |segment|
+      record.errors.add(attribute, "segment #{segment} needs to be a quiz, video or text") unless [Assessment::Quiz, Video, Text].any? { |klass| segment.is_a?(klass) }
     end
   end
 
@@ -20,19 +20,20 @@ class LessonsValidator < ActiveModel::EachValidator
     return unless value.is_a?(Array)
 
     ref_counts = Hash.new(0)
-    value.each { |lesson| ref_counts[lesson.ref] += 1 }
+    value.each { |segment| ref_counts[segment.ref] += 1 }
     non_unique_refs = ref_counts.select { |_, count| count > 1 }.keys
 
-    non_unique_refs.each { |ref| record.errors.add(attribute, "=> ref '#{ref}' is not unique") }
+    non_unique_refs.each { |ref| record.errors.add(attribute, "(#{record.title}) => Segment ref '#{ref}' is not unique") }
   end
 
   def check_unique_title(record, attribute, value)
     return unless value.is_a?(Array)
 
     title_counts = Hash.new(0)
-    value.each { |lesson| title_counts[lesson.title] += 1 } 
+
+    value.each { |segment| title_counts[segment.title] += 1 }
     non_unique_titles = title_counts.select { |_, count| count > 1 }.keys
 
-    non_unique_titles.each { |title| record.errors.add(attribute, "=> Title '#{title}' is not unique") }
+    non_unique_titles.each { |title| record.errors.add(attribute, "(#{record.title}) => Segment title '#{title}' is not unique") }
   end
 end
